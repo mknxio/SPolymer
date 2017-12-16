74de891d88a7bb97aebd9397cd9dea80ac7bcdcb
paulguzda <paulguzdariviere@gmail.com>
2017-12-16 16:57:21 +0100

    Implement an 'HTML imports' parser, that stores valid statements

--- linter.py
+++ linter.py
@@ -7,6 +7,8 @@ class SploymerLinter():
         DocString
     """
 
+    _html_content = ''
+    _html_imports = []
     _errors = []
 
     def lint_html_imports(self, content):
@@ -16,8 +18,30 @@ class SploymerLinter():
             import statements, then will check for valid file paths, and
             display an error tooltip if paths are not valid.
         """
-        rgx_html_import = '(<link +({}|{}).*\/?>)'.format(
-                                            'rel="import" +(.+ +)?(href=".+")',
-                                            '(href=".+") +(.+ +)?rel="import"')
+        self._html_content = content
+        rgx_html_import = '(<link +.*rel=\"import\".*\/?>)'
         matches = re.findall(rgx_html_import, content, re.I)
+        for match in matches:
+            match = match.split(' ')
+            # Remove unwanted withspaces & characters
+            trimd_tag = {}
+            for attr in match:
+                if attr and attr != '>' and attr != '/>':
+                    if attr[:1] == '<':
+                        attr = attr[1:]
+                    if attr[-2:] == '/>':
+                        attr = attr[:-2]
+                    if attr[-1:] == '>':
+                        attr = attr[:-1]
+                    # Build obj. representation of trimmed tag
+                    trimd_tag['_'] = 'link'
+                    if attr != 'link':
+                        attr = attr.split("=")
+                        trimd_tag[attr[0]] = attr[1][1:-1]
+            # Check if the current import statement is valid, then store it
+            has_rel_import = 'rel' in trimd_tag and trimd_tag['rel'] == 'import'
+            has_href = 'href' in trimd_tag and trimd_tag['href']
+            if has_rel_import and has_href:
+                self._html_imports.append(trimd_tag)
+        print(self._html_imports)
 
--- spolymer.py
+++ spolymer.py
@@ -7,12 +7,16 @@ from SPolymer.linter import SploymerLinter
 class Spolymer(sublime_plugin.EventListener):
     """
         Main listener class that handles user events
-        and performs corresponding actions.
+        and launch corresponding processes.
     """
 
     linter = SploymerLinter()
 
     def on_modified(self, view):
+        print('============= SPOLYMER DEBUG =============')
+
         view_content = view.substr(sublime.Region(0, view.size()))
         # Launch lint processes
         self.linter.lint_html_imports(view_content)
+
+        print('========== SPOLYMER DEBUG / END ==========')
